ef242f25aaaee5be7744cbcabed3ceed7db54d0d
ProgramApe6020 <GuoWenHong0804@outlook.com>
2021-10-05 17:49:27 +0800

    Initial build and development.

---
+++ src/org/programape6020/spigot/Days_Nights.java
@@ -0,0 +1,17 @@
+package org.programape6020.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Days_Nights extends JavaPlugin {
+	 @Override
+	 public void onEnable() {
+		 System.out.println("Days Nights已成功加载");
+	 }
+	    
+	 @Override
+	 public void onDisable() {
+		 System.out.println("Days Nights已成功卸载");
+	 }
+}
