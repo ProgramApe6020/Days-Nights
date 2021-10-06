a59118bb27485978542a5960750cb7dcf18aa0aa
ProgramApe6020 <GuoWenHong0804@outlook.com>
2021-10-06 08:21:01 +0800

    Changes ServerListener.java.

--- src/org/programape6020/spigot/ServerListener.java
+++ src/org/programape6020/spigot/ServerListener.java
@@ -1,5 +1,6 @@
 package org.programape6020.spigot;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.entity.Player;
@@ -10,11 +11,13 @@ import java.text.SimpleDateFormat;
 public class ServerListener implements Listener {
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
+		//Clear the original online prompt
+		event.setJoinMessage(null);
 		//Get player ID and send time information
 		Player player_join = event.getPlayer();
 		String player_name = player_join.getName().toString();
-		player_join.sendMessage("Welcome " + player_name + "to the server.");
-		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//Set date format
-        player_join.sendMessage("Now is " + df.format(new Date()));
+		Bukkit.broadcastMessage("Welcome " + player_name + "to the server.");	//Send Message
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");		//Set date format
+		Bukkit.broadcastMessage("Now is " + df.format(new Date()));				//Send Message
 	}
 }
