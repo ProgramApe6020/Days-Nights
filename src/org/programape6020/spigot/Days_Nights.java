e0d7a9153025947b11ca8d3a15c20cb71f9d6216
ProgramApe6020 <GuoWenHong0804@outlook.com>
2021-10-06 08:09:28 +0800

    Just add some notes.

--- src/org/programape6020/spigot/Days_Nights.java
+++ src/org/programape6020/spigot/Days_Nights.java
@@ -3,12 +3,14 @@ package org.programape6020.spigot;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Days_Nights extends JavaPlugin {
+	 //Load plugin execution
 	 @Override
 	 public void onEnable() {
 		 System.out.println("Days Nights loaded successfully");
+		 //Register event listener
 		 getServer().getPluginManager().registerEvents(new ServerListener(), this);
 	 }
-	    
+	 //Disable plugin execution
 	 @Override
 	 public void onDisable() {
 		 System.out.println("Days nights uninstalled successfully");
