69feb52a3c5d61a0b8fb29f69d5be9f13f6cfa29
ProgramApe6020 <GuoWenHong0804@outlook.com>
2021-10-17 16:40:09 +0800

    The function of adjusting time is improved.

--- src/org/programape6020/spigot/Days_Nights.java
+++ src/org/programape6020/spigot/Days_Nights.java
@@ -4,18 +4,44 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.GameRule;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Days_Nights extends JavaPlugin {
 	 //Load plugin execution
+	 static Map<World, Boolean> addDay = new HashMap<>();
 	 @Override
 	 public void onEnable() {
 		 System.out.println("Days Nights loaded successfully");
-		 World world = Bukkit.getWorld(getName());
-		 //Set game time stop
-		 world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+		 World world = Bukkit.getWorld("world");
 		 //Register event listener
 		 getServer().getPluginManager().registerEvents(new ServerListener(), this);
-		 getServer().getPluginManager().registerEvents(new TimeManager(), this);
+		//Set game time stop
+		 world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+		 Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+			 if (!addDay.containsKey(world))
+				 addDay.put(world, true);
+			 Calendar c = Calendar.getInstance();
+			 int hour = c.get(Calendar.HOUR_OF_DAY);  	//Get the current hour
+			 int minute = c.get(Calendar.MINUTE);  		//Get the current minute
+			 int second = c.get(Calendar.SECOND);  		//Get the current second
+			 if(hour<6)
+				 hour+=18;  							//hour = hour + 24 - 6
+			 else
+                hour-=6;
+	         double milliHour = (minute * 60 + second) / 3.6;
+	         int time = (int) (hour * 1000 + milliHour);
+             if (time == 0 && addDay.get(world)) {
+                 time = 24000;
+                 addDay.put(world, false);
+             }else if (time != 0)
+                 addDay.put(world, true);
+             long nowTime = world.getTime();
+             long nowFullTime = world.getFullTime();
+             long newFullTime = nowFullTime + time - nowTime;
+             world.setFullTime(newFullTime);
+		 }, 0L, 5L);
 	 }
 	 //Disable plugin execution
 	 @Override
